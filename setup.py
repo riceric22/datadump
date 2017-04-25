e3458f24ce8dd23cb1c24a4582ecc3d77fde3684
Eric Wong <ericwong@cs.cmu.edu>
2017-04-25 14:01:32 -0400

    added travis auto-deploy and bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='datadump',
-    version='0.0.1',
+    version='0.0.2',
     description="Organized workspaces for simple saving and loading of variables",
     author='Eric Wong',
     author_email='ericwong@cs.cmu.edu',
